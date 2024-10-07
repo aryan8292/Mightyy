086e9ea196c3c29f31e705f128ef0f1bc21c28f2
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-10-07 22:29:08 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -8,7 +8,7 @@ from pyrogram.enums import ChatMemberStatus
 from config import FORCE_SUB_CHANNEL, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
-from shortzy import Shortzy
+import Shortzy
 import requests
 import time
 from datetime import datetime
